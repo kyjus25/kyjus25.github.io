ba9c3cc83121728eeb93497d4b14b37aa7923483
Justin White <kyjus25@gmail.com>
2021-02-10 22:56:49 -0600

    Updated Files

--- src/kyjus25-github-io/src/app/main/left/latest-project/latest-project.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-latest-project',
-  templateUrl: './latest-project.component.html',
-  styleUrls: ['./latest-project.component.css']
-})
-export class LatestProjectComponent {}
